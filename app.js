e846db6c0a98ef7b714e4d137ecd8ac4059329d3
Joel Hellman <joel.hellman11@gmail.com>
2016-07-05 21:05:25 +0200

    added events and html injected examples

--- app.js
+++ app.js
@@ -4,20 +4,35 @@
   return {
     events: {
       'app.activated' : 'onAppActivated',
+      'click [data-action-add-html-template]' : 'onAddHtmlTemplate',
+      'click [data-action-add-hdbs-template]' : 'onAddHdbsTemplate',
       'click [data-action-hdbs-select-set]': 'onBtnSetHdbsSelect',
       'click [data-action-hdbs-select-get]': 'onBtnGetHdbsSelect',
       'click [data-action-html-select-set]': 'onBtnSetHtmlSelect',
-      'click [data-action-html-select-get]': 'onBtnGetHtmlSelect'
+      'click [data-action-html-select-get]': 'onBtnGetHtmlSelect',
+      'zd_ui_change .mySelectHtml':  'onHtmlSelectChanged',
+      'zd_ui_change .mySelectHdbs':  'onHdbsSelectChanged'
     },
-
     onAppActivated: function() {
-      var options = [
+      this.options = [
         { value: 'foo', label: 'FOO'},
         { value: 'bar', label: 'BAR'}
       ];
-      this.switchTo('ticket', {options: options});
+      this.optionSelectedValue = this.options[1].value;
+      this.switchTo('ticket', {options: this.options}); // will initialize zd-type automatically
     },
-
+    onAddHtmlTemplate: function() {
+      this.$('.add-html-template-wrapper').html(this.renderTemplate('_combo_select_html'));
+      this.$('[data-zd-type]').zdComboSelectMenu(); // need to initialize manually
+    },
+    onAddHdbsTemplate: function() {
+      this.$('.add-hdbs-template-wrapper').html(this.renderTemplate('_combo_select_hdbs', {
+          options: this.options
+      }));
+      this.$('[data-zd-type]').zdComboSelectMenu();
+      // need to set the selected value manually since helper doesn't support selected option
+      this.$('.myAddedSelectHdbs > .zd-combo-selectmenu').zdComboSelectMenu('setValue', this.optionSelectedValue);
+    },
     onBtnSetHdbsSelect: function() {
       this.$('.mySelectHdbs > .zd-combo-selectmenu').zdComboSelectMenu('setValue', 'bar');
     },
@@ -29,8 +44,13 @@
     },
     onBtnGetHtmlSelect: function() {
       console.log('Html select value ', this.$('.mySelectHtml').zdComboSelectMenu('value'));
+    },
+    onHtmlSelectChanged: function(evt, change) {
+      console.log('Html select change object ', change);
+    },
+    onHdbsSelectChanged: function(evt, change) {
+      console.log('Hdbs select change object ', change);
     }
-
   };
 
 }());
